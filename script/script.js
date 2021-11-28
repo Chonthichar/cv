d67a8653a15b92aabc182a29470114920dd3d7c9
Chonthichar <chonthichar.soythong@gmail.com>
2021-11-28 12:51:57 +0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const anime = document.querySelectorAll('#timeline li');
+
+const screenView = box => {
+    const action = box.getBoundingClientRect();
+    return (
+        action.top >= 0 && action.left >= 0 && action.bottom <= (window.innerHeight || document.documentElement.clientHeight) && action.right
+        <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+const active = () => anime.forEach(anime => {
+    if (screenView(anime)){
+        anime.classList.add('on')
+    };
+});
+
+window.addEventListener('load', active);
+window.addEventListener('resize', active);
+window.addEventListener('scroll', active);
